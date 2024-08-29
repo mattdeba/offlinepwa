ab34334f6c54af9aaf6a73f727f86de8bd505fbe
Matthieu-D <matthieu.debarge@cuma.fr>
2024-08-29 17:27:55 +0200

    mise à jour de la config de la pwa

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
+
+
+enableProdMode();
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/ngsw-worker.js');
+  });
+}
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
