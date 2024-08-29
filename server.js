a37eecfa928dfe7e6cc1a3aa2f86bd34f79ee309
Matthieu-D <matthieu.debarge@cuma.fr>
2024-08-29 16:55:43 +0200

    initial commit with pwa

---
+++ server.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = process.env.PORT || 8080;
+
+
+app.use('/', express.static(path.join(__dirname, 'dist', 'tfour')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'tfour', 'index.html'));
+});
+
+app.listen(port, () => console.log(`HTTP Server running on port ${port}`));
