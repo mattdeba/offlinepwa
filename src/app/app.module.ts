87e0793d7e199ccce1c6f1d379efe7ba63870d63
matthieu debarge <debarge.matthieu@gmail.com>
2024-09-04 16:45:29 +0200

    ajout du module okta dans l'app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,14 @@ import { MaterialFeatures } from './material/material.module';
 import { FormulaireComponent } from './formulaire/formulaire.component';
 import { AppRoutingModule } from './app-routing.module';
 import { Model } from './model/repository.model';
+import OktaAuth from '@okta/okta-auth-js';
+import { OktaAuthModule } from '@okta/okta-angular';
+
+const oktaAuth = new OktaAuth({
+  issuer: 'https://cuma.okta.com/oauth2/aus38e911jvrUbFhn4x7',
+  clientId: '0oam37b31i1MlMnzu4x7',
+  redirectUri: 'http://localhost:4200/login/callback',
+});
 
 @NgModule({
   declarations: [
@@ -15,7 +23,8 @@ import { Model } from './model/repository.model';
   imports: [
     BrowserModule,
     MaterialFeatures,
-    AppRoutingModule
+    AppRoutingModule,
+    OktaAuthModule.forRoot({ oktaAuth }),
   ],
   providers: [Model],
   bootstrap: [AppComponent]
