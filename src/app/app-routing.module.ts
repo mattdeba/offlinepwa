b331298f97d48b11bb3c36c00ac7a40c3d02ae86
matthieu debarge <debarge.matthieu@gmail.com>
2024-09-19 13:10:06 +0200

    ajout du composant home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormulaireComponent } from './formulaire/formulaire.component';
 import {OktaCallbackComponent} from "@okta/okta-angular";
 import {authGuard} from "./auth/auth.guard";
+import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'formulaire', component: FormulaireComponent },//temporary deactivate guard for development
   { path: 'login/callback', component: OktaCallbackComponent}
 ];
