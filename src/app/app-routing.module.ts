4380bf390069077ac6ddebdaffbcc8daa015f81d
matthieu debarge <debarge.matthieu@gmail.com>
2024-09-19 14:19:40 +0200

    correction du décalage entre les deux titres

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'formulaire', component: FormulaireComponent, canActivate: [authGuard] },
+  { path: 'formulaire', component: FormulaireComponent},//, canActivate: [authGuard] },
   { path: 'login/callback', component: OktaCallbackComponent}
 ];
 
